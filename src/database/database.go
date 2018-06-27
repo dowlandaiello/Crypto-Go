1c4a8925d807e9ee1e3a16ca93af541b576027c0
Wyatt Hauben <whauben11@gmail.com>
2018-06-27 15:00:56 -0400

    Backup.

--- src/database/database.go
+++ src/database/database.go
@@ -1,9 +1,7 @@
 package database
 
 import (
-	"github.com/mitsukomegumi/FakeCrypto/src/accounts"
 	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
 
 // ReadDatabase - attempt to fetch database from specified address
@@ -20,31 +18,4 @@ func ReadDatabase(address string) (*mgo.Database, error) {
 	c := session.DB("crypto")
 
 	return c, nil
-}
-
-// AddAccount - add account to database
-func AddAccount(database *mgo.Database, account accounts.Account) error {
-	c := database.C("accounts")
-
-	err := c.Insert(account)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// FindAccount - query specified database, return found account (if found)
-func FindAccount(database *mgo.Database, account accounts.Account, username string) (accounts.Account, error) {
-	c := database.C("accounts")
-
-	result := accounts.Account{}
-
-	err := c.Find(bson.M{"username": username}).One(&result)
-	if err != nil {
-		return result, err
-	}
-
-	return result, nil
 }
