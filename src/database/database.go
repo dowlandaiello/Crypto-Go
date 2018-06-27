f9aa2e30438a6b9ee4eb462fbdf4c518864819fa
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-06-27 14:52:20 -0400

    Added account reading, database reading.

---
+++ src/database/database.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"github.com/mitsukomegumi/FakeCrypto/src/accounts"
+	"gopkg.in/mgo.v2"
+)
+
+// ReadDatabase - attempt to fetch database from specified address
+func ReadDatabase(address string) (*mgo.Database, error) {
+	session, err := mgo.Dial(address)
+	if err != nil {
+		return nil, err
+	}
+
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("crypto")
+
+	return c, nil
+}
+
+// AddAccount - add account to database
+func addAccount(database *mgo.Database, account accounts.Account) error {
+	c := database.C("accounts")
+
+	err := c.Insert(accounts.NewAccount("test", "test", "test"))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
