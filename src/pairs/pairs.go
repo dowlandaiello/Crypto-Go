6cf9b8bd543f46e2eb55149e211b06c4be50ab44
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-06-27 21:32:42 -0400

    Handled invalid trading pairs.

---
+++ src/pairs/pairs.go
@@ -0,0 +1,17 @@
+package pairs
+
+import "github.com/mitsukomegumi/FakeCrypto/src/common"
+
+// Pair - trading pair definition
+type Pair struct {
+	StartingSymbol string `json:"startingsymbol"`
+	EndingSymbol   string `json:"endingsymbol"`
+}
+
+// NewPair - returns pair, checks if valid
+func NewPair(startingSymbol string, endingSymbol string) Pair {
+	if startingSymbol != endingSymbol && common.StringInSlice(startingSymbol, common.AvailableSymbols) && common.StringInSlice(endingSymbol, common.AvailableSymbols) {
+		return Pair{StartingSymbol: startingSymbol, EndingSymbol: endingSymbol}
+	}
+	return Pair{}
+}
